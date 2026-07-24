b82163a180ea06b52f7920c1636adcce90c01301
aniket-cyber <aniket99bose@gmail.com>
2026-07-24 16:28:09 +0530

    Changes in url for frontend

--- player-auction-frontend/src/environments/environment.prod.ts
+++ player-auction-frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: '/api/v1',
-  socketUrl: '/',
+  apiUrl: 'https://player-auction-v2-backend.onrender.com/api/v1',
+  socketUrl: 'https://player-auction-v2-backend.onrender.com',
 };
